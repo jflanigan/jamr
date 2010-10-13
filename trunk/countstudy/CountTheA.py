84f1193ab457f753c1af7e1e31caf0dc6efc55f9
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2010-10-13 16:53:22 +0000

    In module CountTheA, findNounPhrase_returnDETandHead(str) works
    git-svn-id: svn+ssh://linux.gp.cs.cmu.edu/afs/cs.cmu.edu/user/jmflanig/repos@3 adcbdfe1-51bf-4484-b097-aa5b931b129a

--- trunk/countstudy/CountTheA.py
+++ trunk/countstudy/CountTheA.py
@@ -1,35 +1,31 @@
 #!/usr/bin/python3
 
 import re
+import sys
 
-def findNounPhraseDETandHead(str):
+def findNounPhrase_returnDETandHead(str):
+# Searches for all noun phrases in a string POStagged by the stanford tagger and returns a list
+# of dictionaries that contain entries for the head and determiner of the noun phrases
     DET = '((?P<DET>\w*)_DT)?'
-    ADJs = '\s*(\w*_JJ)*'
-    Ns = '\s*(\w*_((NN)|(NNS)|(NNP)|(NNPS)))*'
+    ADJs = '(\s*\w*_JJ)*'
+    Ns = '(\s*\w*_((NN)|(NNS)|(NNP)|(NNPS)))*'
     HEAD = '\s*(?P<HEAD>\w*)_((NN)|(NNS)|(NNP)|(NNPS))'
     regexp = DET + ADJs + Ns + HEAD
-    for match in re.finditer(regexp, str)
-        match.groupdict()
+    return [ match.groupdict() for match in re.finditer(regexp, str) ]
 
+def findHead_returnDET(str, head):
+    DET = '((?P<DET>\w*)_DT)?'
+    ADJs = '(\s*\w*_JJ)*'
+    Ns = '(\s*\w*_((NN)|(NNS)|(NNP)|(NNPS)))*'
+    HEAD = '\s*(' + head + ')_((NN)|(NNS)|(NNP)|(NNPS))'
+    regexp = DET + ADJs + Ns + HEAD
+    return [ match.groupdict()['DET'] for match in re.finditer(regexp, str) ]
 
+def run():
+    ref = open(sys.argv[1], 'r').readlines()
+    #mt = open(sys.argv[2], 'r').readlines()
+    
+    print(ref[0])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#run()
 
