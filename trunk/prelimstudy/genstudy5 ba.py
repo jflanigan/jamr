38416948852a5346c95bc2291e9b115598cb1b64
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2010-10-13 00:23:34 +0000

    Initial import
    git-svn-id: svn+ssh://linux.gp.cs.cmu.edu/afs/cs.cmu.edu/user/jmflanig/repos@1 adcbdfe1-51bf-4484-b097-aa5b931b129a

---
+++ trunk/prelimstudy/genstudy5 ba.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import random
+import sys
+# The following should work for python3.0
+#enlist = open('en', 'r').readlines()
+#chlist = open('ch', 'r').readlines()
+#outlist = open('ch_output', 'w')
+
+# The following should work for python 2.6
+import codecs
+enlist = codecs.open('en', 'r', encoding='utf-8').readlines()
+chlist = codecs.open('ch', 'r', encoding='utf-8').readlines()
+out = codecs.open('ch_output', 'w', encoding='utf-8')
+
+if len(enlist) != len(chlist):
+    print 'Error len(en) = ', len(enlist), ', len(ch) = ', len(chlist)
+
+filtered = []
+for (en,ch,line) in zip(enlist, chlist, range(1, len(enlist))):
+    if ( #(ch.find(u'有') != -1 and (' '+en).find(' a ') != -1 and len(en) < 80) # 1448 (182 less than 40 in ch)
+      (ch.find(u'把') != -1 and len(en) < 80) # 538 (116 less than 40 in ch)
+      and en.find(' er ') == -1 and en.find(' uh ') == -1 and en.find(' um ') == -1):  # 2917 (171 less than 30 in ch)
+#    if len(en) < 80:
+        filtered.append((en,ch,line))
+#        print ch
+#        print en
+
+sys.stdout = codecs.open('study5.txt', 'w', encoding='utf-8')
+
+for i in range(0,30):
+    x = random.choice(range(0,len(filtered)))
+    (en,ch,line) = filtered[x]
+    print i, ': Line ', line
+    print ch
+    out.write(ch+u'\n')
+    print ''  #pinyin
+    print ''
+    print ''  #google translate
+    print ''
+    print en
+    print '\n'
+
+print len(enlist), ' sentences total in the corpus.'
+print 'Selection process (for definiteness effects and length), filtered this down to ', len(filtered), ' sentences.'
+print 'The above sentences were randomly selected from the ', len(filtered), 'filtered sentences.'
+
+
+sys.stdout.close()
+
