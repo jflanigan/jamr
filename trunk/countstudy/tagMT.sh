5554fe7d4ef65be514eed688614cd3fa6f788591
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2010-11-01 05:35:44 +0000

    tagMT.sh current directory bugfix
    git-svn-id: svn+ssh://linux.gp.cs.cmu.edu/afs/cs.cmu.edu/user/jmflanig/repos@35 adcbdfe1-51bf-4484-b097-aa5b931b129a

--- trunk/countstudy/tagMT.sh
+++ trunk/countstudy/tagMT.sh
@@ -4,13 +4,13 @@
 
 # (first run Tokenize.py)
 
-../stanford-postagger-newline.sh models/left3words-wsj-0-18.tagger output.hiero/hiero.tokens > output.hiero/hiero.tagged
+../stanford-postagger-newline.sh models/left3words-wsj-0-18.tagger $PWD/output.hiero/hiero.tokens > output.hiero/hiero.tagged
 
-../stanford-postagger-newline.sh models/left3words-wsj-0-18.tagger output.phrase.based/phrase.tokens > output.phrase.based/phrase.tagged
+../stanford-postagger-newline.sh models/left3words-wsj-0-18.tagger $PWD/output.phrase.based/phrase.tokens > output.phrase.based/phrase.tagged
 
-../stanford-postagger-newline.sh models/left3words-wsj-0-18.tagger ref/ref1.tokens > ref/ref1.tagged
-../stanford-postagger-newline.sh models/left3words-wsj-0-18.tagger ref/ref2.tokens > ref/ref2.tagged
-../stanford-postagger-newline.sh models/left3words-wsj-0-18.tagger ref/ref3.tokens > ref/ref3.tagged
-../stanford-postagger-newline.sh models/left3words-wsj-0-18.tagger ref/ref4.tokens > ref/ref4.tagged
+../stanford-postagger-newline.sh models/left3words-wsj-0-18.tagger $PWD/ref/ref1.tokens > ref/ref1.tagged
+../stanford-postagger-newline.sh models/left3words-wsj-0-18.tagger $PWD/ref/ref2.tokens > ref/ref2.tagged
+../stanford-postagger-newline.sh models/left3words-wsj-0-18.tagger $PWD/ref/ref3.tokens > ref/ref3.tagged
+../stanford-postagger-newline.sh models/left3words-wsj-0-18.tagger $PWD/ref/ref4.tokens > ref/ref4.tagged
 
 
