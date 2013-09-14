e8c4deb5740e0e9010365366a63632b5eaa05160
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2013-09-14 23:01:29 +0000

    Delete
    git-svn-id: svn+ssh://linux.gp.cs.cmu.edu/afs/cs.cmu.edu/user/jmflanig/repos@713 adcbdfe1-51bf-4484-b097-aa5b931b129a

--- amr-parser/src/package.scala
+++
@@ -1,8 +0,0 @@
-package edu.cmu.lti.nlp
-
-package object amr {
-    //implicit def doubleToMulAssoc(x: Double) = new MulAssoc(x)
-    var verbosity = 1
-    def logger(n: Int, s: Any) { if(n<=verbosity) System.err.println(s) }
-}
-
