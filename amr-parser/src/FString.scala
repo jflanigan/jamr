2580af5a18eba699f7b4923e2c890d6c342295ab
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2013-10-09 23:17:11 +0000

    About to fix FString
    git-svn-id: svn+ssh://linux.gp.cs.cmu.edu/afs/cs.cmu.edu/user/jmflanig/repos@807 adcbdfe1-51bf-4484-b097-aa5b931b129a

--- amr-parser/src/FString.scala
+++ amr-parser/src/FString.scala
@@ -1,5 +1,6 @@
 package edu.cmu.lti.nlp.amr
 
+import scala.io.Source
 import scala.util.matching.Regex
 import scala.collection.mutable.Map
 import scala.collection.mutable.Set
@@ -14,7 +15,7 @@ class FString private (private val list: List[Int]) {   // private constructor a
         List(FString.strings.size-1)
     })
     def + (fstring: FString) : FString = { // it is effecient to do fstr1+fstr2+fstr3 but not fstr1+(fstr2+fstr3)
-        new FString(fstring.list ::: list)
+        new FString(fstring.list ::: this.list)
     }
     override def toString() : String = {
         "FString("+mkString(":")+")"
@@ -29,6 +30,40 @@ object FString {
     val lookup: Map[String, Int] = Map.empty[String, Int]
     def apply(str: String) : FString = {    // Factory method for creating an FString
         new FString(str)
+    }
+
+/***************** Demo *******************/
+    def main(args: Array[String]) {
+        if(args.size != 1) {
+            println("usage: scala -classpath . edu.cmu.lti.nlp.amr.FString ../aligner/snt")
+            sys.exit(1)
+        }
+
+        val corpus : Array[String] = for {line <- Source.fromFile(args(0)).getLines.toArray
+             i <- line.split(" +")} yield i
+
+        { // Test 1 (fast)
+        println("Starting test 1")
+//        val tokens = ArrayBuffer.empty[FString]
+        val unigrams = Map.empty[FString, Double]
+        val bigrams = Map.empty[FString, Double]
+        val trigrams = Map.empty[FString, Double]
+
+        var prev1, prev2 = FString("")
+        for (i <- corpus) {
+            val token = FString(i)
+            unigrams += (token -> (unigrams.getOrElse(token, 0.0)+1))
+            bigrams += ((token + prev1) -> (bigrams.getOrElse(token + prev1, 0.0)+1))
+            trigrams += ((token + prev1 + prev2) -> (trigrams.getOrElse(token + prev1 + prev2, 0.0)+1))
+            prev2 = prev1
+            prev1 = token
+        }
+        println("Tokens: " + corpus.size.toString)
+        println("Unigrams: " + unigrams.size.toString)
+        println("Bigrams: " + bigrams.size.toString)
+        println("Trigrams: " + trigrams.size.toString)
+        }
+
     }
 }
 
