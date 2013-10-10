1a55ebf961207fc07b143d30a7b3db3d4dbfb1da
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2013-10-10 02:30:29 +0000

    Add graph decoder
    git-svn-id: svn+ssh://linux.gp.cs.cmu.edu/afs/cs.cmu.edu/user/jmflanig/repos@808 adcbdfe1-51bf-4484-b097-aa5b931b129a

--- amr-parser/src/FString.scala
+++ amr-parser/src/FString.scala
@@ -6,7 +6,7 @@ import scala.collection.mutable.Map
 import scala.collection.mutable.Set
 import scala.collection.mutable.ArrayBuffer
 
-class FString private (private val list: List[Int]) {   // private constructor and private member list
+case class FString private (private val list: List[Int]) {   // private constructor and private member list
     private def this(str: String) = this(if (FString.lookup.contains(str)) {    // create an FString by doing FString("string")
         List(FString.lookup(str))
     } else {
@@ -15,7 +15,7 @@ class FString private (private val list: List[Int]) {   // private constructor a
         List(FString.strings.size-1)
     })
     def + (fstring: FString) : FString = { // it is effecient to do fstr1+fstr2+fstr3 but not fstr1+(fstr2+fstr3)
-        new FString(fstring.list ::: this.list)
+        FString(fstring.list ::: list)
     }
     override def toString() : String = {
         "FString("+mkString(":")+")"
@@ -42,9 +42,11 @@ object FString {
         val corpus : Array[String] = for {line <- Source.fromFile(args(0)).getLines.toArray
              i <- line.split(" +")} yield i
 
+        for (x <- Range(0,10)) {
         { // Test 1 (fast)
-        println("Starting test 1")
-//        val tokens = ArrayBuffer.empty[FString]
+        println("Starting Test 1")
+        val startTime = System.currentTimeMillis
+        val tokens = ArrayBuffer.empty[FString]
         val unigrams = Map.empty[FString, Double]
         val bigrams = Map.empty[FString, Double]
         val trigrams = Map.empty[FString, Double]
@@ -52,6 +54,7 @@ object FString {
         var prev1, prev2 = FString("")
         for (i <- corpus) {
             val token = FString(i)
+            tokens += token
             unigrams += (token -> (unigrams.getOrElse(token, 0.0)+1))
             bigrams += ((token + prev1) -> (bigrams.getOrElse(token + prev1, 0.0)+1))
             trigrams += ((token + prev1 + prev2) -> (trigrams.getOrElse(token + prev1 + prev2, 0.0)+1))
@@ -62,8 +65,60 @@ object FString {
         println("Unigrams: " + unigrams.size.toString)
         println("Bigrams: " + bigrams.size.toString)
         println("Trigrams: " + trigrams.size.toString)
+
+        val size = tokens.size
+        print("Performing 1 million lookups... ")
+        for (n <- Range(0,1000000)) {
+            val word1 = tokens((Math.random()*size).toInt)
+            val word2 = tokens((Math.random()*size).toInt)
+            val word3 = tokens((Math.random()*size).toInt)
+            unigrams.getOrElse(word1, 0.0)
+            bigrams.getOrElse(word1 + word2, 0.0)
+            trigrams.getOrElse(word1 + word2 + word3, 0.0)
+        }
+        println("done")
+        val endTime = System.currentTimeMillis
+        println(endTime-startTime)
         }
 
+        { // Test 2
+        println("Starting Test 2")
+        val startTime = System.currentTimeMillis
+        val tokens = ArrayBuffer.empty[String]
+        val unigrams = Map.empty[String, Double]
+        val bigrams = Map.empty[String, Double]
+        val trigrams = Map.empty[String, Double]
+
+        var prev1, prev2 = ""
+        for (i <- corpus) {
+            val token = i
+            tokens += token
+            unigrams += (token -> (unigrams.getOrElse(token, 0.0)+1))
+            bigrams += ((token + ":" + prev1) -> (bigrams.getOrElse(token + prev1, 0.0)+1))
+            trigrams += ((token + ":" + prev1 + ":" + prev2) -> (trigrams.getOrElse(token + ":" + prev1 + ":" + prev2, 0.0)+1))
+            prev2 = prev1
+            prev1 = token
+        }
+        println("Tokens: " + corpus.size.toString)
+        println("Unigrams: " + unigrams.size.toString)
+        println("Bigrams: " + bigrams.size.toString)
+        println("Trigrams: " + trigrams.size.toString)
+
+        val size = tokens.size
+        print("Performing 1 million lookups... ")
+        for (n <- Range(0,1000000)) {
+            val word1 = tokens((Math.random()*size).toInt)
+            val word2 = tokens((Math.random()*size).toInt)
+            val word3 = tokens((Math.random()*size).toInt)
+            unigrams.getOrElse(word1, 0.0)
+            bigrams.getOrElse(word1 + ":" + word2, 0.0)
+            trigrams.getOrElse(word1 + ":" + word2 + ":" + word3, 0.0)
+        }
+        println("done")
+        val endTime = System.currentTimeMillis
+        println(endTime-startTime)
+        }
+        }
     }
 }
 
---
+++ amr-parser/src/GraphDecoder/Decoder.scala
@@ -0,0 +1,38 @@
+package edu.cmu.lti.nlp.amr.GraphDecoder
+import edu.cmu.lti.nlp.amr._
+
+import java.io.File
+import java.io.FileOutputStream
+import java.io.PrintStream
+import java.io.BufferedOutputStream
+import java.io.OutputStreamWriter
+import java.lang.Math.abs
+import java.lang.Math.log
+import java.lang.Math.exp
+import java.lang.Math.random
+import java.lang.Math.floor
+import java.lang.Math.min
+import java.lang.Math.max
+import scala.io.Source
+import scala.util.matching.Regex
+import scala.collection.mutable.Map
+import scala.collection.mutable.Set
+import scala.collection.mutable.ArrayBuffer
+
+case class Dependency(head: Int, dependent: Int, relation: String)
+
+case class Input(graph: Graph, sentence: Array[String], parse: Array[Dependency])
+
+case class DecoderResult(graph: Graph, /*features: FeatureVector,*/ score: Double)
+
+abstract class Decoder() {
+    type Label = String
+
+    var weight: (Node, Node, Label, Input) => Double
+    var nodes: Array[Node]
+    var labels: Array[Label]
+
+    def decode(input: Input) : Unit
+
+}
+
