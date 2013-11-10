e010dab2cdd4e11e3588bff4481d1e27d8b2702d
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2013-11-10 02:20:45 +0000

    Fix some things in annotation class
    git-svn-id: svn+ssh://linux.gp.cs.cmu.edu/afs/cs.cmu.edu/user/jmflanig/repos@873 adcbdfe1-51bf-4484-b097-aa5b931b129a

--- amr-parser/src/Annotation.scala
+++ amr-parser/src/Annotation.scala
@@ -18,40 +18,52 @@ import scala.collection.mutable.Map
 import scala.collection.mutable.Set
 import scala.collection.mutable.ArrayBuffer
 
-case class Annotation[T](tokenized: Array[String], myTokenized: Array[String], get: T) {
+case class Annotation[T](tok: Array[String], annotationTok: Array[String], get: T) {
     // This class can be used for annotations on the tokens of a sentence.
     // The annotations can use a different tokenization scheme, and getSpan can be used to convert
     // a span 'myTokenized' to a span in 'tokenized'.
     // Public member 'get' is the annotation.
 
-    assert(tokenized.mkString == myTokenized.mkString, "Annotation tokenization schemes do not match")
-    assert(tokenized.mkString.count(_ == ' ') == 0, "Spaces not allowed in tokens") // because we count spaces to find the left and right indices
+    assert(tok.mkString == annotationTok.mkString, "Annotation tokenization schemes do not match")
+    assert(tok.mkString.count(_ == ' ') == 0, "Spaces not allowed in tokens") // because we count spaces to find the left and right indices
+    assert(annotationTok.mkString.count(_ == ' ') == 0, "Spaces not allowed in tokens") // because we count spaces to find the left and right indices
 
-    val left = myTokenized.map(x => 0)
-    val right = myTokenized.map(x => 0)
+    def makeLeftRight(tokenized: Array[String], myTokenized: Array[String]) : (Array[Int], Array[Int]) = {
+        val left = myTokenized.map(x => 0)
+        val right = myTokenized.map(x => 0)
 
-    for (i <- Range(0, myTokenized.size)) {
-        val regexr = myTokenized.take(i+1).mkString.split("").drop(1).mkString(" ?").r
-        regexr.findPrefixOf(tokenized.mkString(" ")) match {
-            case Some(prefix) => { println(prefix); println(prefix.count(_ == ' ')); right(i) = prefix.count(_ == ' ') + 1}
-            case None => assert(false, "Error matching the prefix (this should never occur)")
-        }
-        if (i > 0) {
-            val regexl = (myTokenized.take(i).mkString.split("").drop(1).mkString(" ?")+" ").r
-            regexl.findPrefixOf(tokenized.mkString(" ")) match {
-                case Some(prefix) => { left(i) = right(i-1) }
-                case None => { left(i) = right(i-1) - 1 }
+        for (i <- Range(0, myTokenized.size)) {
+            val regexr = myTokenized.take(i+1).mkString.split("").drop(1).mkString(" ?").r
+            regexr.findPrefixOf(tokenized.mkString(" ")) match {
+                case Some(prefix) => { println(prefix); println(prefix.count(_ == ' ')); right(i) = prefix.count(_ == ' ') + 1}
+                case None => assert(false, "Error matching the prefix (this should never occur)")
+            }
+            if (i > 0) {
+                val regexl = (myTokenized.take(i).mkString.split("").drop(1).mkString(" ?")+" ").r
+                regexl.findPrefixOf(tokenized.mkString(" ")) match {
+                    case Some(prefix) => { left(i) = right(i-1) }
+                    case None => { left(i) = right(i-1) - 1 }
+                }
             }
         }
+        println(right.toList)
+        println(left.toList)
+        return (left, right)
     }
 
-    println(right.toList)
-    println(left.toList)
+    val (left, right) = makeLeftRight(tok, annotationTok)
+    val (leftinv, rightinv) = makeLeftRight(annotationTok, tok)
 
     def getSpan(span: (Int, Int)) : (Int, Int) = {
-    // Input: Span in myTokenized (indices start from 0, and span goes from span._1 to span._2-1 inclusive ie same as slice(Int,Int))
-    // Output: Span in tokenized (indices start from 0, and span goes from ret._1 to ret._2-1 inclusive ie same as slice(Int,Int))
-        return (left(span._1), right(span._2))
+        // Input: Span in annotationTok (indices start from 0, and span goes from span._1 to span._2-1 inclusive ie same as slice(Int,Int))
+        // Output: Span in tok (indices start from 0, and span goes from ret._1 to ret._2-1 inclusive ie same as slice(Int,Int))
+        return (left(span._1), right(span._2-1))
+    }
+
+    def toAnnotationSpan(span: (Int, Int)) : (Int, Int) = {
+        // Input: Span in tok
+        // Output: Span in annotationTok
+        return (leftinv(span._1), rightinv(span._2-1))
     }
 }
 
@@ -61,30 +73,31 @@ class AnnotationTest /*extends Suite*/ {
                                               Array("The", "Riyadh-based", "Naif", "Arab", "Academy"),
                                               Array())
         println("test1")
-        println("tokenized = "+test1.tokenized.toList)
-        println("myTokenized = "+test1.myTokenized.toList)
-        for (i <- Range(0, test1.myTokenized.size)) {
-            println("i=" + i + " l=" + test1.getSpan(i,i)._1 + " r=" + test1.getSpan(i,i)._2)
+        println("tokenized = "+test1.tok.toList)
+        println("myTokenized = "+test1.annotationTok.toList)
+        for (i <- Range(0, test1.annotationTok.size)) {
+            println("i=" + i + " l=" + test1.getSpan(i,i+1)._1 + " r=" + test1.getSpan(i,i+1)._2)
         }
         val test2 = Annotation[Array[String]](Array("The", "Riyadh-based", "NaifArab", "Academy"),
                                               Array("The", "Riyadh", "-", "based", "Naif", "ArabAcademy"),
                                               Array())
         println("test2")
-        println("tokenized = "+test2.tokenized.toList)
-        println("myTokenized = "+test2.myTokenized.toList)
-        for (i <- Range(0, test2.myTokenized.size)) {
-            println("i=" + i + " l=" + test2.getSpan(i,i)._1 + " r=" + test2.getSpan(i,i)._2)
+        println("tokenized = "+test2.tok.toList)
+        println("myTokenized = "+test2.annotationTok.toList)
+        for (i <- Range(0, test2.annotationTok.size)) {
+            println("i=" + i + " l=" + test2.getSpan(i,i+1)._1 + " r=" + test2.getSpan(i,i+1)._2)
         }
+        println("getSpan(1,4)="+test2.getSpan(1,4))
+        println("getSpan(1,5)="+test2.getSpan(1,5))
         val test3 = Annotation[Array[String]](Array("The", "Riyadh-", "based", "NaifArab", "Academy"),
                                               Array("The", "Riy", "adh", "-", "based", "Naif", "ArabAcademy"),
                                               Array())
         println("test3")
-        println("tokenized = "+test3.tokenized.toList)
-        println("myTokenized = "+test3.myTokenized.toList)
-        for (i <- Range(0, test3.myTokenized.size)) {
-            println("i=" + i + " l=" + test3.getSpan(i,i)._1 + " r=" + test3.getSpan(i,i)._2)
+        println("tokenized = "+test3.tok.toList)
+        println("myTokenized = "+test3.annotationTok.toList)
+        for (i <- Range(0, test3.annotationTok.size)) {
+            println("i=" + i + " l=" + test3.getSpan(i,i+1)._1 + " r=" + test3.getSpan(i,i+1)._2)
         }
-
 
     }
 }
