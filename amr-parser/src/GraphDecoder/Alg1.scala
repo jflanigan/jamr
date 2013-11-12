587ed20fe39591ca448eeecab2e8766d1f5131ba
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2013-11-12 05:38:20 +0000

    Add GraphObj.  Alg1 now keeps track of connectivity
    git-svn-id: svn+ssh://linux.gp.cs.cmu.edu/afs/cs.cmu.edu/user/jmflanig/repos@885 adcbdfe1-51bf-4484-b097-aa5b931b129a

--- amr-parser/src/GraphDecoder/Alg1.scala
+++ amr-parser/src/GraphDecoder/Alg1.scala
@@ -28,36 +28,38 @@ class Alg1(featureNames: List[String], labelSet: Array[(String, Int)], rootedCon
     def decode(input: Input) : DecoderResult = {
         // Assumes that Node.relations has been setup correctly for the graph fragments
         val graph = input.graph.duplicate
-        val nodes : List[Node] = graph.nodes.toList
+        val nodes : List[Node] = graph.nodes.filter(_.name != None).toList    // TODO: test to see if a view is faster
+        val graphObj = new GraphObj(graph, features)    // graphObj keeps track of the connectivity of the graph as we add edges
         features.input = input
-
-        def neighbors(node: Node) : List[Node] = {
-            nodes
-        }
 
         logger(1, "Alg1")
         //logger(2, "weights = " + features.weights)
 
-        var score = 0.0
-        val feats = new FeatureVector()
-        for { node1 <- nodes
-              relations = node1.relations.map(_._1).toSet
+        for { (node1, index1) <- nodes.zipWithIndex
+              relations = node1.relations.map(_._1)
               (label, maxCardinality) <- labelSet } {
 
-            if (relations.contains(label)) {
-                for ((_, node2) <- node1.relations.filter(_._1 == label)) {
-                    feats += features.localFeatures(node1, node2, label)
-                    score += features.localScore(node1, node2, label)
-                }
-            } else {
-                // Search over neighbors, and pick the ones with highest score
-                val nodes2 : List[(Node, Double)] = neighbors(node1).map(x => (x, features.localScore(node1, x, label))).filter(x => x._2 > 0 && x._1.id != node1.id).sortBy(-_._2).take(maxCardinality)
+            if (relations.count(_ ==label) == 0) {   // relations.count(_ == label) counts the edges that are already in the graph fragments
+                // Search over the nodes, and pick the ones with highest score
+                val nodes2 : List[(Node, Int, Double)] = nodes.zipWithIndex.map(x => (x._1, x._2, features.localScore(node1, x._1, label))).filter(x => x._3 > 0 && x._1.id != node1.id).sortBy(-_._3).take(maxCardinality)
 
-                for ((node2, weight) <- nodes2) {
-                    node1.relations = (label, node2) :: node1.relations
-                    feats += features.localFeatures(node1, node2, label)
-                    score += weight
+                for ((node2, index2, weight) <- nodes2) {
                     logger(1, "Adding edge ("+node1.concept+", "+label +", "+node2.concept + ") with weight "+weight.toString)
+                    graphObj.addEdge(node1, index1, node2, index2, label, weight)
+                }
+                if (nodes2.size > 0) {
+                    logger(2, "node1 = " + node1.concept)
+                    logger(2, "label = " + label)
+                    logger(2, "nodes2 = " + nodes.toString)
+                    //logger(1, "feats = " + feats.toString)
+                }
+            } else if (relations.count(_ == label) < maxCardinality) {   // relations.count(_ == label) counts the edges that are already in the graph fragments
+                // Search over the nodes, and pick the ones with highest score
+                val relationIds : List[String] = node1.relations.map(_._2.id)   // we assume if there is a relation already in the fragment, we can't add another relation type between the two nodes
+                val nodes2 : List[(Node, Int, Double)] = nodes.zipWithIndex.map(x => (x._1, x._2, features.localScore(node1, x._1, label))).filter(x => x._3 > 0 && x._1.id != node1.id && !relationIds.contains(x._1.id)).sortBy(-_._3).take(maxCardinality - relations.count(_ == label))
+                for ((node2, index2, weight) <- nodes2) {
+                    logger(1, "Adding edge ("+node1.concept+", "+label +", "+node2.concept + ") with weight "+weight.toString)
+                    graphObj.addEdge(node1, index1, node2, index2, label, weight)
                 }
                 if (nodes2.size > 0) {
                     logger(2, "node1 = " + node1.concept)
@@ -73,11 +75,12 @@ class Alg1(featureNames: List[String], labelSet: Array[(String, Int)], rootedCon
         } else {
             graph.root = nodes(0)
         }
-        feats += features.rootFeatures(graph.root)
+        graphObj.feats += features.rootFeatures(graph.root)
+        graphObj.score += features.rootScore(graph.root)
 
         nodes.map(node => { node.relations = node.relations.reverse })
         //graph.makeTopologicalOrdering()   // won't work - may not be connected
-        return DecoderResult(graph, feats, score)
+        return DecoderResult(graph, graphObj.feats, graphObj.score)
     }
 }
 
