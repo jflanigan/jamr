7358e312d7926e839b8a117aa6c2407745616be1
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2013-11-14 07:16:06 +0000

    Version used for weights.20 and weights.21
    git-svn-id: svn+ssh://linux.gp.cs.cmu.edu/afs/cs.cmu.edu/user/jmflanig/repos@904 adcbdfe1-51bf-4484-b097-aa5b931b129a

--- amr-parser/src/AMRParser.scala
+++ amr-parser/src/AMRParser.scala
@@ -168,7 +168,7 @@ scala -classpath . edu.cmu.lti.nlp.amr.AMRParser -w weights -l labelset < input 
                            logger(0, "Oracle:\n"+result1.graph.printTriples(
                                 detail = 1,
                                 extra = (node1, node2, relation) => {
-                                    "\t"+decoder.features.ffDependencyPath(node1, node2, relation).toString.split("\n").filter(_.matches("^C1.*")).toList.toString+"\t"+decoder.features.localScore(node1, node2, relation).toString
+                                    "\t"+decoder.features.ffDependencyPathv2(node1, node2, relation).toString.split("\n").filter(_.matches("^C1.*")).toList.toString+"\t"+decoder.features.localScore(node1, node2, relation).toString
                                 })+"\n")
                        }
                        val amrdata2 = AMRData(training(i))
@@ -183,7 +183,7 @@ scala -classpath . edu.cmu.lti.nlp.amr.AMRParser -w weights -l labelset < input 
                            logger(0, result2.graph.printTriples(
                                 detail = 1,
                                 extra = (node1, node2, relation) => {
-                                    "\t"+oracle.features.ffDependencyPath(node1, node2, relation).toString.split("\n").filter(_.matches("^C1.*")).toList.toString+"\t"+decoder.features.localScore(node1, node2, relation).toString
+                                    "\t"+oracle.features.ffDependencyPathv2(node1, node2, relation).toString.split("\n").filter(_.matches("^C1.*")).toList.toString+"\t"+decoder.features.localScore(node1, node2, relation).toString
                                 })+"\n")
                        }
                        logger(0, "Dependencies:\n"+dependencies(i)+"\n")
@@ -227,11 +227,11 @@ scala -classpath . edu.cmu.lti.nlp.amr.AMRParser -w weights -l labelset < input 
                 val decoderResult = decoder.decode(new Input(amrdata, dependencies(i), oracle = false))
                 val oracleResult = oracle.decode(new Input(amrdata2, dependencies(i), oracle = true))
                 logger(0, "Oracle:\n"+oracleResult.graph.printTriples(detail = 1, extra = (node1, node2, relation) => {
-                    "\t"+oracle.features.ffDependencyPath(node1, node2, relation).toString.split("\n").filter(_.matches("^C1.*")).toList.toString+"\t"+decoder.features.localScore(node1, node2, relation).toString
+                    "\t"+oracle.features.ffDependencyPathv2(node1, node2, relation).toString.split("\n").filter(_.matches("^C1.*")).toList.toString+"\t"+decoder.features.localScore(node1, node2, relation).toString
                     //"\n"+oracle.features.ffDependencyPath(node1, node2, relation).toString.split("\n").filter(_.matches("^C1.*")).toList.toString+"\nScore = "+decoder.features.localScore(node1, node2, relation).toString+"  Relevent weights:\n"+decoder.features.weights.slice(decoder.features.localFeatures(node1, node2, relation)).toString
                 })+"\n")
                 logger(0, "AMR:\n"+decoderResult.graph.printTriples(detail = 1, extra = (node1, node2, relation) => {
-                    "\t"+decoder.features.ffDependencyPath(node1, node2, relation).toString.split("\n").filter(_.matches("^C1.*")).toList.toString+"\t"+decoder.features.localScore(node1, node2, relation).toString
+                    "\t"+decoder.features.ffDependencyPathv2(node1, node2, relation).toString.split("\n").filter(_.matches("^C1.*")).toList.toString+"\t"+decoder.features.localScore(node1, node2, relation).toString
                     //"\n"+decoder.features.ffDependencyPath(node1, node2, relation).toString.split("\n").filter(_.matches("^C1.*")).toList.toString+"\nScore = "+decoder.features.localScore(node1, node2, relation).toString+"  Relevent weights:\n"+decoder.features.weights.slice(decoder.features.localFeatures(node1, node2, relation)).toString
                 })+"\n")
                 if (outputFormat.contains("AMR")) {
