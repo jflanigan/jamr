917c3234aeb9794c3c622a603fc1e23f8ece1ae4
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2013-11-16 19:08:30 +0000

    Same bug fig for Alg1 (in GraphObj)
    git-svn-id: svn+ssh://linux.gp.cs.cmu.edu/afs/cs.cmu.edu/user/jmflanig/repos@913 adcbdfe1-51bf-4484-b097-aa5b931b129a

--- amr-parser/src/GraphDecoder/GraphObj.scala
+++ amr-parser/src/GraphDecoder/GraphObj.scala
@@ -77,111 +77,15 @@ case class GraphObj(graph: Graph,
     logger(1, "Adding edges already there")
     val nodeIds : Array[String] = nodes.map(_.id).toArray
     for { (node1, index1) <- nodes.zipWithIndex
-          (label, node2) <- node1.relations
-          if nodeIds.indexWhere(_ == node2.id) != -1 } {
-        val index2 = nodeIds.indexWhere(_ == node2.id)
-        addEdge(node1, index1, node2, index2, label, features.localScore(node1, node2, label), addRelation=false)
+          (label, node2) <- node1.relations } {
+        if (nodeIds.indexWhere(_ == node2.id) != -1) {
+            val index2 = nodeIds.indexWhere(_ == node2.id)
+            addEdge(node1, index1, node2, index2, label, features.localScore(node1, node2, label), addRelation=false)
+        } else {
+            feats += features.localFeatures(node1, node2, label)
+            score += features.localScore(node1, node2, label)
+        }
     }
 
 }
-
-/*
-    def decode(input: Input) : DecoderResult = {
-        // Assumes that Node.relations has been setup correctly for the graph fragments
-        val graph = input.graph.duplicate
-        features.input = input
-        //val nodes : Array[Node] = graph.nodes.filter.toArray
-        val nodes : Array[Node] = graph.nodes.filter(_.name != None).toArray
-        //val nonDistinctLabels = labelSet.toList.filter(x => x._2 > 1) // TODO: remove
-        val nonDistinctLabels : Array[(String, Int)] = new Array(0)
-        logger(2,"ndLabels = "+nonDistinctLabels.toList)
-        //val distinctLabels = labelSet.filter(x => x._2 == 1)  // TODO: remove
-        val distinctLabels = labelSet
-
-        logger(1, "Adding edges already there")
-        val nodeIds : Array[String] = nodes.map(_.id)
-        for { (node1, index1) <- nodes.zipWithIndex
-              (label, node2) <- node1.relations
-              if nodeIds.indexWhere(_ == node2.id) != -1 } {
-            val index2 = nodeIds.indexWhere(_ == node2.id)
-            addEdge(node1, index1, node2, index2, label, features.localScore(node1, node2, label), addRelation=false)
-        }
-
-        logger(1, "set = " + set.toList)
-        logger(1, "nodes = " + nodes.map(x => x.concept).toList)
-        logger(1, "setArray = " + setArray.toList)
-
-        logger(1, "Adding positive edges")
-        val neighbors : Array[Array[(String, Double)]] = {
-            for ((node1, index1) <- nodes.zipWithIndex) yield {
-                for ((node2, index2) <- nodes.zipWithIndex if (index1 != index2)) yield {
-                    val (label, weight) = distinctLabels.map(x => (x._1, features.localScore(node1, node2, x._1))).maxBy(_._2)
-                    logger(1,"distinctLabels = "+distinctLabels.map(x => (x._1, features.localScore(node1, node2, x._1))).sortBy(-_._2).toList.take(5)+"...")
-                    logger(1,"label = "+label)
-                    logger(1,"weight = "+weight)
-                    val ndLabels = nonDistinctLabels.map(x => (x._1, features.localScore(node1, node2, x._1))).filter(x => x._2 > 0 && x._1 != label)
-                    logger(2,"ndLabels = "+nonDistinctLabels.map(x => (x._1, features.localScore(node1, node2, x._1))))
-                    logger(2,"ndLabels = "+ndLabels.toList)
-                    ndLabels.filter(_._2 > 0).map(x => addEdge(node1, index1, node2, index2, x._1, x._2))
-                    if (weight > 0) {   // Add all positive weights
-                        addEdge(node1, index1, node2, index2, label, weight)
-                    }
-                    if (ndLabels.size > 0) {
-                        val (label2, weight2) = ndLabels.maxBy(_._2)
-                        if (weight > weight2) {
-                            logger(1, "1neighbors("+node1.concept+","+node2.concept+")="+label)
-                            (label, weight)
-                        } else {
-                            logger(1, "2neighbors("+node1.concept+","+node2.concept+")="+label2)
-                            (label2, weight2)
-                        }
-                    } else {
-                        logger(1, "neighbors("+node1.concept+","+node2.concept+")="+label)
-                        (label, weight)
-                    }
-                }
-            }
-        }
-
-        // Add negative weights to the queue
-        logger(1, "Adding negative edges")
-        val queue = new PriorityQueue[(Double, Int, Int, String)]()(Ordering.by(x => -x._1))
-        if (connected && set.size != 0 && getSet(0).size != nodes.size) {
-            for { (node1, index1) <- nodes.zipWithIndex
-                  ((label, weight), index2) <- neighbors(index1).zipWithIndex
-                  if index1 != index2 && weight <= 0 && set(index1) != set(index2) } {
-                queue.enqueue((weight, index1, index2, label))
-            }
-        }
-
-        // Kruskal's algorithm
-        if (connected) {    // if we need to produce a connected graph
-            logger(1, "queue = " + queue.toString)
-            logger(1, "set = " + set.toList)
-            logger(1, "nodes = " + nodes.map(x => x.concept).toList)
-            logger(1, "setArray = " + setArray.toList)
-            while (set.size != 0 && getSet(0).size != nodes.size) {
-                logger(2, queue.toString)
-                val (weight, index1, index2, label) = queue.dequeue
-                if (set(index1) != set(index2)) {
-                    addEdge(nodes(index1), index1, nodes(index2), index2, label, weight)
-                }
-            }
-        }
-
-        if (features.rootFeatureFunctions.size != 0) {
-            graph.root = nodes.map(x => (x, features.rootScore(x))).maxBy(_._2)._1
-        } else {
-            logger(1, "Setting root to "+nodes(0).id)
-            graph.root = nodes(0)
-        }
-        feats += features.rootFeatures(graph.root)
-
-        nodes.map(node => { node.relations = node.relations.reverse })
-        if (connected) {
-            graph.makeTopologicalOrdering()
-        }
-        return DecoderResult(graph, feats, score)
-    } */
-
 
