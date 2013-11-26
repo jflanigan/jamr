d07782facf51f4122fbadd27df80d27dcb2e3e1b
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2013-11-26 21:40:08 +0000

    Add concept invoke directory
    git-svn-id: svn+ssh://linux.gp.cs.cmu.edu/afs/cs.cmu.edu/user/jmflanig/repos@926 adcbdfe1-51bf-4484-b097-aa5b931b129a

---
+++ amr-parser/src/ConceptInvoke/Decoder.scala
@@ -0,0 +1,29 @@
+package edu.cmu.lti.nlp.amr.ConceptInvoke
+import edu.cmu.lti.nlp.amr._
+
+import java.io.File
+import java.io.FileOutputStream
+import java.io.PrintStream
+import java.io.BufferedOutputStream
+import java.io.OutputStreamWriter
+import java.lang.Math.abs
+import java.lang.Math.log
+import java.lang.Math.exp
+import java.lang.Math.random
+import java.lang.Math.floor
+import java.lang.Math.min
+import java.lang.Math.max
+import scala.io.Source
+import scala.util.matching.Regex
+import scala.collection.mutable.Map
+import scala.collection.mutable.Set
+import scala.collection.mutable.ArrayBuffer
+
+case class DecoderResult(graph: Graph, features: FeatureVector, score: Double)
+
+abstract class Decoder(featureNames: List[String]) {
+    val features = new Features(featureNames) // maybe this should be renamed ff?
+
+    def decode(input: Input) : DecoderResult
+}
+
