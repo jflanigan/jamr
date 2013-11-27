3a83aae35f76571f6bf9335147a105e06a8afcdf
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2013-11-27 08:52:58 +0000

    Still working on concept invoker
    git-svn-id: svn+ssh://linux.gp.cs.cmu.edu/afs/cs.cmu.edu/user/jmflanig/repos@929 adcbdfe1-51bf-4484-b097-aa5b931b129a

--- amr-parser/src/ConceptInvoke/Decoder1.scala
+++ amr-parser/src/ConceptInvoke/Decoder1.scala
@@ -16,13 +16,17 @@ import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.PriorityQueue
 import Double.{NegativeInfinity => minusInfty}
 
+/*** Defined in package.scala ***
+type PhraseConceptPair = (List[String], String, PhraseConceptFeatures)
+********************************/
+
 class Decoder1(featureNames: List[String],
-               phraseConceptPairs: Array[(List[String], String, PhraseFeatures)])
+               phraseConceptPairs: Array[PhraseConceptPair])
     extends Decoder(featureNames) {
     // Base class has defined:
     // val features: Features
 
-    val conceptTable: Map[String, List[(List[String], String, PhraseFeatures)]] = Map()   // maps the first word in the phrase to a list of phraseConceptPairs
+    val conceptTable: Map[String, List[PhraseConceptPair]] = Map()  // maps the first word in the phrase to a list of phraseConceptPairs
     for ((phrase, graphFragment, feats) <- phraseConceptPairs) {
         val word = phrase(0)
         conceptTable(word) = (phrase, graphFragment, feats) :: conceptTable.getOrElse(word, List())
@@ -30,9 +34,35 @@ class Decoder1(featureNames: List[String],
 
     def decode(input: Input) : DecoderResult = {
         val sentence = input.sentence
+        val bestState : Array[Option[(Double, PhraseConceptPair, Int)]] = sentence.map(x => None)    // (score, concept, backpointer)
         for (i <- Range(0, sentence.size)) {
             var conceptList = conceptTable(sentence(i)).filter(x => x._1 == sentence.slice(i, i+x._1.size).toList)
+            // WARNING: the code below assumes that anything in the conceptList will not extend beyond the end of the sentence (and it shouldn't based on the code above)
+            for (concept <- conceptList) {
+                val score = features.localScore(input, concept)
+                val endpoint = i + concept._1.size - 1
+                if (score >= 0 || bestState(endpoint) == None || bestState(endpoint).get._1 <= score) { // we use <= so that later concepts (i.e. ones our conceptTable) have higher priority
+                    bestState(endpoint) = Some((score, concept, i))
+                }
+            }
         }
+
+        // Follow backpointers
+        val graph = Graph.empty
+        var score = 0.0
+        val feats = new FeatureVector()
+        var i = bestState.size - 1
+        while (i >= 0) {
+            if (bestState(i) != None) {
+                val (localScore, concept, backpointer) = bestState(i)
+                graph.addSpan(backpointer, i+1, concept._2)
+                feats += features.localFeatures(input, concept)
+                score += localScore
+                i = backpointer
+            }
+            i -= 1
+        }
+        return DecoderResult(graph, feats, score)
     }
 
 
