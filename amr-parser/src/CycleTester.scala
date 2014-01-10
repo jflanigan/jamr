3a2e06d79ed90a146dcec467e03750b3f589d754
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2014-01-10 02:39:59 +0000

    Add Sam's CycleTester
    git-svn-id: svn+ssh://linux.gp.cs.cmu.edu/afs/cs.cmu.edu/user/jmflanig/repos@975 adcbdfe1-51bf-4484-b097-aa5b931b129a

---
+++ amr-parser/src/CycleTester.scala
@@ -0,0 +1,60 @@
+package edu.cmu.lti.nlp.amr
+
+import scala.annotation.tailrec
+
+// Written by Sam Thomsom
+
+object CycleTester {
+  /**
+    Takes a directed graph, as a set of nodes and a map from node to its out-adjacent nodes,
+    and determines whether the graph contains a cycle.
+   */
+  @tailrec
+  def hasCycle[T](nodes: Traversable[T], outgoingEdges: Map[T, Traversable[T]]): Boolean = {
+    if (outgoingEdges.isEmpty) {
+      false
+    } else {
+      val oFirstLeaf: Option[T] = nodes.toIterator.find(v => outgoingEdges.getOrElse(v, Nil).isEmpty).headOption
+      oFirstLeaf match {
+        case None => true
+        case Some(node) => {
+          val removed = (outgoingEdges - node) map { case (k, v) => (k, v.toSet - node) }
+          hasCycle(nodes.toSet - node, removed)
+        }
+      }
+    }
+  }
+
+  def main(args: Array[String]) {
+    val graphA = Map(
+      1 -> Set(2),
+      2 -> Set(3),
+      3 -> Set(1)
+    )
+    println(hasCycle(1 to 3, graphA) + " should be true")
+
+    val graphB = Map(
+      1 -> Set(2, 3),
+      2 -> Set(3)
+    )
+    println(hasCycle(1 to 3, graphB) + " should be false")
+
+    val graphC = Map(
+      1 -> Set(2),
+      2 -> Set(3, 4),
+      4 -> Set(5, 6),
+      5 -> Set(6),
+      6 -> Set(3)
+    )
+    println(hasCycle(1 to 6, graphC) + " should be false")
+
+    val graphD = Map(
+      1 -> Set(2),
+      2 -> Set(3, 4),
+      4 -> Set(5),
+      5 -> Set(6),
+      6 -> Set(3, 4)
+    )
+    println(hasCycle(1 to 6, graphD) + " should be true")
+  }
+}
