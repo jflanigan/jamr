ba44f3ee041a8ca2ad1bbc38e38a5f9b0c005cb6
Sam Thomson <sammthomson@gmail.com>
2014-06-18 21:42:58 -0400

    run script bugfix

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # assumes this script (config.sh) lives in "${JAMR_HOME}/scripts/"
-JAMR_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." > /dev/null && pwd )"
+export JAMR_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." > /dev/null && pwd )"
 
-CLASSPATH=".:${JAMR_HOME}/target/scala-2.10/jamr-assembly-0.1-SNAPSHOT.jar"
+export CLASSPATH=".:${JAMR_HOME}/target/scala-2.10/jamr-assembly-0.1-SNAPSHOT.jar"
 
-DATA_DIR="${JAMR_HOME}/data/LDC-2013-Sep"
+export DATA_DIR="${JAMR_HOME}/data/LDC-2013-Sep"
 
-MODEL_DIR="${JAMR_HOME}/experiments/current"
+export MODEL_DIR="${JAMR_HOME}/experiments/current"
 
-CDEC="${HOME}/code/cdec" # CHANGEME
+export CDEC="${HOME}/code/cdec" # CHANGEME
 
-ILLINOIS_NER="${HOME}/code/IllinoisNerExtended-v2.3" # CHANGEME
+export ILLINOIS_NER="${HOME}/code/IllinoisNerExtended-v2.3" # CHANGEME
 
-STANFORD_PARSER="${HOME}/code/stanford-parser-2011-09-14/stanford-parser.jar" # CHANGEME
+export STANFORD_PARSER="${HOME}/code/stanford-parser-2011-09-14/stanford-parser.jar" # CHANGEME
