696b7cdf3d90ab23560c66275024c220929e7a6e
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2014-07-05 21:09:18 -0400

    update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,13 +2,15 @@
 
 # assumes this script (config.sh) lives in "${JAMR_HOME}/scripts/"
 
+# change the following enviroment variables for your configuration
+
 export CDEC="${HOME}/tools/cdec" # CHANGEME
 
 export ILLINOIS_NER="${HOME}/tools/IllinoisNerExtended" # CHANGEME
 
 export ILLINOIS_NER_JAR="${ILLINOIS_NER}/dist/LbjNerTagger-2.3.jar" # CHANGEME
 
-# the settings below shouldn't need to be changed
+# the variables below shouldn't need to be changed
 
 export JAMR_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." > /dev/null && pwd )"
 
