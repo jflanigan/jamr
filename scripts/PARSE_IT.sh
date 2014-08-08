6ceff0770e54e2405af95d10ed6935dbf569ddea
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2014-08-08 19:03:42 -0400

    Fix for files not in local dir

--- scripts/PARSE_IT.sh
+++ scripts/PARSE_IT.sh
@@ -13,12 +13,13 @@ cat "$INPUT" | "${CDEC}/corpus/tokenize-anything.sh" > "$INPUT.tok"
 #### NER ####
 
 inputfile="$INPUT"
-outputfile="$INPUT.IllinoisNER.tmp"
+outputfile="$PWD/$INPUT.IllinoisNER.tmp"
 configfile="$JAMR_HOME/scripts/preprocessing/IllinoisNER.config"
 cpath="$ILLINOIS_NER_JAR:$ILLINOIS_NER/lib/*"
 cat $inputfile | sed 's/$/\n####\n/' > $inputfile.tmp
+temp="$PWD/$inputfile.tmp"
 pushd "$ILLINOIS_NER"
-java -classpath  ${cpath} -Xmx8g edu.illinois.cs.cogcomp.LbjNer.LbjTagger.NerTagger -annotate $inputfile.tmp ${outputfile} ${configfile}
+java -classpath  ${cpath} -Xmx8g edu.illinois.cs.cogcomp.LbjNer.LbjTagger.NerTagger -annotate ${temp} ${outputfile} ${configfile}
 popd
 cat "$outputfile" | sed 's/ #### /\n/g' | "$JAMR_HOME/src/IllinoisNERConvert" | head -n -2 > "$INPUT.IllinoisNER"
 rm "$outputfile"
