968dd51767641a9e59d4b9e202683206ae32b413
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2014-10-06 22:38:26 -0400

    add config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,22 +2,17 @@
 
 # assumes this script (config.sh) lives in "${JAMR_HOME}/scripts/"
 
-# change the following enviroment variables for your configuration
-
-export CDEC="${HOME}/tools/cdec" # CHANGEME
-
-export ILLINOIS_NER="${HOME}/tools/IllinoisNerExtended" # CHANGEME
-
-export ILLINOIS_NER_JAR="${ILLINOIS_NER}/target/IllinoisNerExtended-2.7.jar" # CHANGEME
-
-export WNHOME="${HOME}/tools/WordNet-3.0" # WORDNET
-
 export JAMR_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." > /dev/null && pwd )"
-
-export DATA_DIR="${JAMR_HOME}/data/LDC-2013-Sep"
-
 export CLASSPATH=".:${JAMR_HOME}/target/scala-2.10/jamr-assembly-0.1-SNAPSHOT.jar"
 
+# change the following enviroment variables for your configuration
+
+export DATA_DIR="${JAMR_HOME}/data/LDC-2013-Sep"
 export MODEL_DIR="${JAMR_HOME}/experiments/current"
 
+export CDEC="${HOME}/tools/cdec"
+export ILLINOIS_NER="${HOME}/tools/IllinoisNerExtended"
+export ILLINOIS_NER_JAR="${ILLINOIS_NER}/target/IllinoisNerExtended-2.7.jar"
+export WNHOME="${HOME}/tools/WordNet-3.0"
 export SMATCH="${JAMR_HOME}/scripts/smatch_v1_0/smatch_modified.py"
+
