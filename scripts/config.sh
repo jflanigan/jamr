fd425f7a334ad1f68b0413c8353372b4ad4b340b
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2014-10-29 12:54:20 -0400

    Update config file

--- scripts/config.sh
+++ scripts/config.sh
@@ -10,9 +10,10 @@ export CLASSPATH=".:${JAMR_HOME}/target/scala-2.10/jamr-assembly-0.1-SNAPSHOT.ja
 export DATA_DIR="${JAMR_HOME}/data/LDC-2013-Sep"
 export MODEL_DIR="${JAMR_HOME}/experiments/current"
 
-export CDEC="${HOME}/tools/cdec"
-export ILLINOIS_NER="${HOME}/tools/IllinoisNerExtended"
+export CDEC="${JAMR_HOME}/tools/cdec"
+export ILLINOIS_NER="${JAMR_HOME}/tools/IllinoisNerExtended"
 export ILLINOIS_NER_JAR="${ILLINOIS_NER}/target/IllinoisNerExtended-2.7.jar"
-export WNHOME="${HOME}/tools/WordNet-3.0"
+export WNHOME="${JAMR_HOME}/tools/WordNet-3.0"
+export SCALA="${JAMR_HOME}/tools/scala-2.11.2/bin/scala"
 export SMATCH="${JAMR_HOME}/scripts/smatch_v1_0/smatch_modified.py"
 
