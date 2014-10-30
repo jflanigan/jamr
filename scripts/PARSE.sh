164e36ec109ab1a524a39259bf6f1a83e8cf6204
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2014-10-29 20:44:37 -0400

    Print nodes and edges by default

--- scripts/PARSE.sh
+++ scripts/PARSE.sh
@@ -53,7 +53,7 @@ ${JAMR_HOME}/run AMRParser \
   --ner "${INPUT}.IllinoisNER" \
   --tok "${INPUT}.tok" \
   --ignore-parser-errors \
-  --output-format AMR,nodes,edge,roots \
+  --output-format AMR,nodes,edges,root \
   -v 0 \
   < "${INPUT}"
 
