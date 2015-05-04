ef458556d20f12f6bfc7770a774d979629831f2f
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2015-05-04 01:59:05 -0400

    splitStates compiles

--- src/graph/Hypergraph.scala
+++ src/graph/Hypergraph.scala
@@ -64,7 +64,6 @@ class Hypergraph[N,E] private (_bareNodes: Array[N], _bareEdges: Array[BareEdge[
         inEdges
     }
     private val _terminals : List[Node[N]] = _nodes.filter(n => incomingEdges(n) == List() || !incomingEdges(n).contains((e : Edge[N,E]) => { e.src != List() })).toList
-    private val goalNodes : List[Node[N]] = _node.filter(n => outgoingEdges(n) == List()).toList
 
     //private def outgoingEdges(srcIndex: Int) : List[Edge[N,E]] = _outgoingEdges(srcIndex)     // Not needed
     //private def incomingEdges(destIndex: Int) : List[Edge[N,E]] = _incomingEdges(destIndex)
@@ -78,36 +77,65 @@ class Hypergraph[N,E] private (_bareNodes: Array[N], _bareEdges: Array[BareEdge[
     def outgoingEdges(src: Node[N]) : List[Edge[N,E]] = _outgoingEdges(src.index)
     def incomingEdges(dest: Node[N]) : List[Edge[N,E]] = _incomingEdges(dest.index)
 
+/*
     def splitStates[M](goalNode: N => Boolean,
                        split: SplitEdge[N,M,E] => BareEdge[M,E]) : Hypergraph[M,E] = {
         val nodesToProcess : List[(Node[N], SplitEdge[N,M,E])] = List()
         for (
         return splitStates(_terminals.map(e => SplitEdge(e.dest, List(), e.get)), split)
-    }
+    } */
 
-    def splitStates[M](goalNode: N => Boolean,
-                       split: SplitEdge[N,M,E] => BareEdge[M,E]) : Hypergraph[M,E] = {
+    def splitStates[M:ClassTag](goalNode: N => Boolean,
+                                split: SplitEdge[N,M,E] => BareEdge[M,E]) : Hypergraph[M,E] = {
         val goalNodes : List[Node[N]] = _terminals.filter(n => goalNode(n.get))
+        val splitNodes : m.Map[Int, List[M]] = m.Map()
         if (goalNodes.size == 1) {
-            return Hypergraph.fromEdges(splitStates(goalNode(0), split)._2)
+            return Hypergraph.fromEdges(splitStates(goalNodes(0), split, splitNodes)._2)
         }
         var edges : List[BareEdge[M,E]] = List()
         for (goal <- goalNodes) {
-            edges = splitStates(goal, split) ::: edges
+            edges = splitStates(goal, split, splitNodes)._2 ::: edges
         }
         return Hypergraph.fromEdges(edges.distinct)
     }
 
     def splitStates[M](node: Node[N],
-                       split: SplitEdge[N,M,E] => BareEdge[M,E]) : (List[M], List[BareEdge[M,E]]) = {
-        if (nodes == List()) {
-            return List()
+                       split: SplitEdge[N,M,E] => BareEdge[M,E],
+                       splitNodes: m.Map[Int, List[M]]) : (List[M], List[BareEdge[M,E]]) = {
+        if (splitNodes.contains(node.index)) {
+            return (splitNodes(node.index), List())
         }
-        for (edge <- outgoingEdges(node)) {
-            
+        var edges : List[BareEdge[M,E]] = List()
+        val myStates : m.Set[M] = m.Set()
+        for (edge <- incomingEdges(node)) {
+            var sources : List[List[M]] = List()
+            for { src <- edge.src
+                  (newStates, newEdges) = splitStates(src, split, splitNodes)
+                } {
+                    edges = newEdges ::: edges
+                    sources = newStates :: sources
+            }
+            sources = sources.reverse
+            // for each seq of source states, run splitNodes
+            for (source <- getProduct(sources)) {    // Make sure that if it's passed and empty list, it returns List(List())
+                val newEdge : BareEdge[M,E] = split(SplitEdge(edge.dest, source.toVector, edge.get))    // TODO: get rid of toVector
+                myStates += newEdge.dest
+                edges = newEdge :: edges
+            }
         }
+        splitNodes(node.index) = myStates.toList
+        return (splitNodes(node.index), edges)
     }
 
+    // see http://stackoverflow.com/questions/13567543/cross-product-of-arbitrary-number-of-lists-in-scala
+    // TODO: make tail recursive
+    private def getProduct[M](input: List[List[M]]) : List[List[M]] = input match{
+        case Nil => List(List())            // just in case you input an empty list
+        case head :: Nil => head.map(_ :: Nil)
+        case head :: tail => for (elem <- head; sub <- getProduct(tail)) yield elem::sub
+    }
+
+/*
     @tailrec
     def splitStates[M,F](processedNodes: i.Set[Int],
                          processedEdges: List[BareEdge[M,E]] = List(),
@@ -130,7 +158,7 @@ class Hypergraph[N,E] private (_bareNodes: Array[N], _bareEdges: Array[BareEdge[
             }
         }
         return splitStates(myProcessedEdges, futureNodes, split)
-    }
+    } */
 
     /*def toCdecFormat(goalNode: N => Boolean, edgeToString: Edge[N,E] => String) : String = {
         write(_nodes.size + " " + _edges.size)
