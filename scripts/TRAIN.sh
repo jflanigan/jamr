ddc2aa151e74a48aa7d577c6b2d492a39398081f
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2015-05-26 17:19:32 -0400

    Add scripts

--- scripts/TRAIN.sh
+++ scripts/TRAIN.sh
@@ -20,9 +20,23 @@ echo "Training stage 1"
 echo "Training stage 2"
 ./cmd.stage2-weights
 
+# Evaluate on dev set
+echo ""
+echo "  ----- Evaluation on Dev: Smatch (all stages) -----" | tee "${MODEL_DIR}/RESULTS.txt"
+./cmd.dev.decode.allstages
+"${JAMR_HOME}/scripts/smatch_v1_0/smatch_modified.py" --pr -f "${MODEL_DIR}/dev.decode.allstages" "${DEV_FILE}" 2>&1 | tee -a "${MODEL_DIR}/RESULTS.txt"
+echo "" | tee -a "${MODEL_DIR}/RESULTS.txt"
+#echo "  ----- Evaluation on Dev: Smatch (gold concept ID) -----" | tee -a "${MODEL_DIR}/RESULTS.txt"
+#./cmd.dev.decode.stage2only
+#"${JAMR_HOME}/scripts/smatch_v1_0/smatch_modified.py" --pr -f "${MODEL_DIR}/dev.decode.stage2only" "${DEV_FILE}" 2>&1 | tee -a "${MODEL_DIR}/RESULTS.txt"
+#echo "" | tee -a "${MODEL_DIR}/RESULTS.txt"
+echo "  ----- Evaluation on Dev: Spans -----" | tee -a "${MODEL_DIR}/RESULTS.txt"
+tail -n 3 "${MODEL_DIR}/dev.decode.allstages.err" | tee -a "${MODEL_DIR}/RESULTS.txt"
+echo ""
+
 # Evaluate on test set
 echo ""
-echo "  ----- Evaluation on Test: Smatch (all stages) -----" | tee "${MODEL_DIR}/RESULTS.txt"
+echo "  ----- Evaluation on Test: Smatch (all stages) -----" | tee -a "${MODEL_DIR}/RESULTS.txt"
 ./cmd.test.decode.allstages
 "${JAMR_HOME}/scripts/smatch_v1_0/smatch_modified.py" --pr -f "${MODEL_DIR}/test.decode.allstages" "${TEST_FILE}" 2>&1 | tee -a "${MODEL_DIR}/RESULTS.txt"
 echo "" | tee -a "${MODEL_DIR}/RESULTS.txt"
