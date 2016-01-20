ee4f0837a18280d2c08a66496a2063c98848ea6d
Jeffrey Flanigan <jflanigan@cs.cmu.edu>
2016-01-20 03:00:28 -0500

    Update TRAIN.sh

--- scripts/TRAIN.sh
+++ scripts/TRAIN.sh
@@ -9,25 +9,25 @@ if [ -z "$JAMR_HOME" ]; then
 fi
 
 pushd "$JAMR_HOME/scripts/preprocessing"
-#./PREPROCESS.sh
+./PREPROCESS.sh
 popd
 
 # Train
 pushd "$JAMR_HOME/scripts/training"
-#./cmd.conceptTable.train
+./cmd.conceptTable.train
 echo "Training stage 1"
-#./cmd.stage1-weights
+./cmd.stage1-weights
 echo "Training stage 2"
-#./cmd.stage2-weights
+./cmd.stage2-weights
 
 # Evaluate on dev set
 echo ""
 echo "  ----- Evaluation on Dev: Smatch (all stages) -----" | tee "${MODEL_DIR}/RESULTS.txt"
-#./cmd.dev.decode.allstages
+./cmd.dev.decode.allstages
 "${JAMR_HOME}/scripts/smatch_v1_0/smatch_modified.py" --pr -f "${MODEL_DIR}/dev.decode.allstages" "${DEV_FILE}" 2>&1 | tee -a "${MODEL_DIR}/RESULTS.txt"
 echo "" | tee -a "${MODEL_DIR}/RESULTS.txt"
 echo "  ----- Evaluation on Dev: Smatch (gold concept ID) -----" | tee -a "${MODEL_DIR}/RESULTS.txt"
-#./cmd.dev.decode.stage2only
+./cmd.dev.decode.stage2only
 "${JAMR_HOME}/scripts/smatch_v1_0/smatch_modified.py" --pr -f "${MODEL_DIR}/dev.decode.stage2only" "${DEV_FILE}" 2>&1 | tee -a "${MODEL_DIR}/RESULTS.txt"
 echo "" | tee -a "${MODEL_DIR}/RESULTS.txt"
 echo "  ----- Evaluation on Dev: Smatch (oracle) -----" | tee -a "${MODEL_DIR}/RESULTS.txt"
@@ -41,11 +41,11 @@ echo "" | tee -a "${MODEL_DIR}/RESULTS.txt"
 # Evaluate on test set
 echo "" | tee -a "${MODEL_DIR}/RESULTS.txt"
 echo "  ----- Evaluation on Test: Smatch (all stages) -----" | tee -a "${MODEL_DIR}/RESULTS.txt"
-#./cmd.test.decode.allstages
+./cmd.test.decode.allstages
 "${JAMR_HOME}/scripts/smatch_v1_0/smatch_modified.py" --pr -f "${MODEL_DIR}/test.decode.allstages" "${TEST_FILE}" 2>&1 | tee -a "${MODEL_DIR}/RESULTS.txt"
 echo "" | tee -a "${MODEL_DIR}/RESULTS.txt"
 echo "  ----- Evaluation on Test: Smatch (gold concept ID) -----" | tee -a "${MODEL_DIR}/RESULTS.txt"
-#./cmd.test.decode.stage2only
+./cmd.test.decode.stage2only
 "${JAMR_HOME}/scripts/smatch_v1_0/smatch_modified.py" --pr -f "${MODEL_DIR}/test.decode.stage2only" "${TEST_FILE}" 2>&1 | tee -a "${MODEL_DIR}/RESULTS.txt"
 echo "  ----- Evaluation on Test: Smatch (oracle) -----" | tee -a "${MODEL_DIR}/RESULTS.txt"
 ./cmd.test.decode.oracle
